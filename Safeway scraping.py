b33dcccdcb62a2c3aa7beec51d119f2b20ef9940
Elliott-Hendrickson <100046451+Elliott-Hendrickson@users.noreply.github.com>
2024-02-18 00:08:40 -0800

    Making Safeway Filtering A Standard Function

--- Safeway scraping.py
+++ Safeway scraping.py
@@ -3,16 +3,19 @@ import re
 
 from bs4 import BeautifulSoup
 
-pricepattern = r'\$\d+\.\d{2}'
+def parseSafeway(keyWord):
+    pricepattern = r'\$\d+\.\d{2}'
+    
+    safeway = getWebsiteHttps(keyWord)[2]
+    
+    soup = BeautifulSoup(safeway, 'html.parser')
+    
+    
+    product_price_divs = soup.find_all('div', class_='product-price')
+    product_title_divs = soup.find_all('div', class_='product-title__text')
 
-safeway = getWebsiteHttps("milk")[0]
-
-soup = BeautifulSoup(safeway, 'html.parser')
-
-
-product_price_divs = soup.find_all('div', class_='product-price')
-product_title_divs = soup.find_all('div', class_='product-title__text')
-
-for product, price in zip(product_title_divs, product_price_divs):
-    print(price.text[11:16] + ":" + product.text)
-
+    outputList = []
+    
+    for product, price in zip(product_title_divs, product_price_divs):
+        outputList.append(price.text[11:16] + ":" + product.text)
+    return outputList
